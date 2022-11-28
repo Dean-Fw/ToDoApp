6c3e86bbda6cfa053f86e20044e6a2342c984665
Dean-Fw <dfw662@gmail.com>
2022-11-28 00:05:08 +0000

    Added all navbar options to nav bar

--- kivy_work/main.py
+++ kivy_work/main.py
@@ -1,9 +1,11 @@
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
-from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.scrollview import MDScrollView
+from kivy.properties import ObjectProperty
 
-class ContentNavigationDrawer(MDBoxLayout):
-    pass
+class ContentNavigationDrawer(MDScrollView):
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
 
 
 class MainApp(MDApp):
