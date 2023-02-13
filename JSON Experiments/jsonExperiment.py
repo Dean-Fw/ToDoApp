5180f93c3ca112576ea997574b489e78c5230d75
Dean-Fw <dfw662@gmail.com>
2023-02-13 20:41:41 +0000

    Added ability to add and remove objects from JSON file

--- JSON Experiments/jsonExperiment.py
+++ JSON Experiments/jsonExperiment.py
@@ -2,17 +2,21 @@ import json
 
 # class to store and display data for a todo list object
 class ToDoList:
+    
     def __init__(self, ListName, Tasks):
         self.list_name = ListName
         self.tasks = Tasks
+    
     def print_data(self):
         print(f"List Name: {self.list_name}\n Tasks: \n     {self.tasks}\n")
 
 # method to open and load data from a JSON file        
 class JsonData:
+    
     def __init__(self, FilePath):
         self.file_path = FilePath
         self.data = self.open_and_load_JSON()
+    
     def open_and_load_JSON(self):
         try:
             f = open(self.file_path)
@@ -25,26 +29,63 @@ class JsonData:
         except:
             print("ERROR: Could not open JSON file")
             exit()
+    
     def load_data(self):
         ToDo_lists = []
         for i in self.data["lists"]:
             saved_list = ToDoList(i["list_name"],i["tasks"])
             ToDo_lists.append(saved_list)
         return ToDo_lists
-    def save_data(self, new_list):
-        with open(self.file_path, 'r+') as f:
-            json_data = json.load(f)
-            json_data["lists"].append(new_list)
-            f.seek(0)
-            json.dump(json_data, f, indent = 4)
-     
+    
+    def save_changes(self, type_of_change):
+            with open(self.file_path, type_of_change) as f:
+                f.seek(0)
+                json.dump(self.data, f, indent = 4)
+                f.flush()
+    
+    def append_new_list(self, new_list):
+        self.data["lists"].append(new_list)
+        self.save_changes("r+")
+
+    def remove_list(self, list_name):
+        for i in range(len(self.data["lists"])):
+            if self.data["lists"][i]["list_name"] == list_name:
+                del self.data["lists"][i]
+        self.save_changes("w")
+
+    def append_new_task(self, new_task, parent_list):
+        for i in range(len(self.data["lists"])):
+            if self.data["lists"][i]["list_name"] == parent_list:
+                self.data["lists"][i]["tasks"].append(new_task)
+        self.save_changes("w")
+
+    def remove_task(self, task_to_remove, parent_list):
+        for i in range(len(self.data["lists"])):
+            if self.data["lists"][i]["list_name"] == parent_list:
+                for j in range(len(self.data["lists"][i]["tasks"])):
+                    if self.data["lists"][i]["tasks"][j]["task_name"] == task_to_remove:
+                        del self.data["lists"][i]["tasks"][j]
+        self.save_changes("w")
+
 def main():
     json_data_obj = JsonData("data.json")
+    
+    #json_data_obj.remove_list("school tasks")
+    #new_list = {"list_name":"school tasks", "tasks":[{"task_name":"Do Homework","completed":False}]}
+    #json_data_obj.append_new_list(new_list) 
+
+    #new_task = {"task_name": "do laundry", "completed": False}
+    #json_data_obj.append_new_task(new_task, "home tasks")
+    json_data_obj.remove_task("do laundry", "home tasks")
     loaded_lists = json_data_obj.load_data()
+     
     for i in loaded_lists:
         print(f"List Name: {i.list_name} \n Tasks:\n    {i.tasks}\n")
-    new_list = {"list_name":"school tasks", "tasks":[{"task_name":"Do Homework","completed":False}]}
-    json_data_obj.save_data(new_list)
     
+    
+    
+   
+   
+
 main()
 
