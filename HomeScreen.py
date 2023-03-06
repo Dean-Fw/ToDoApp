c97f2149774458d7e1c8fbdf7b2e6c5a660542a3
Dean-Fw <dfw662@gmail.com>
2023-03-06 10:31:44 +0000

    Home UI scaled properly

--- HomeScreen.py
+++ HomeScreen.py
@@ -7,7 +7,7 @@ class HomeScreen(MDScreen):
 
 class FavouriteSpace(MDBoxLayout):
     def calc_height(self):
-        height = len(self.ids.space_for_cards.children) * 150
-        return str(height) + "dp"
+        height = len(self.ids.space_for_cards.children) * 100
+        return str(height + 60) + "dp"
         
                 
